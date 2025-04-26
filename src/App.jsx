a460a62c2b2789e0c6678e21e09106d081360209
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-26 17:51:00 +0600

    Navbar is almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  const isOwnerPath = useLocation().pathname.includes("owner");
+  return <div className="">{!isOwnerPath && <Navbar />}</div>;
 };
 
 export default App;
