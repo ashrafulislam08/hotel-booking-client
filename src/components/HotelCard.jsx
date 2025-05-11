bd2baf94eef473c1450c4d496720a4cf8a4bd665
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-11 16:29:18 +0600

    featured destinations section done

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -8,12 +8,9 @@ const HotelCard = ({ room, index }) => {
       to={`/rooms/` + room._id}
       onClick={() => scrollTo(0, 0)}
       key={room._id}
+      className="relative max-w-70 w-full rounded-xl overflow bg-white shadow-[0px_4px_4px_rgba(0, 0, 0, 0.05)]"
     >
-      <img
-        src={room.images[0]}
-        alt=""
-        className="relative max-w-70 w-full rounded-xl overflow bg-white shadow-[0px_4px_4px_rgba(0, 0, 0, 0.05)]"
-      />
+      <img src={room.images[0]} alt="" className="rounded-xl" />
       {index % 2 === 0 && (
         <p className="px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full">
           Best Seller
